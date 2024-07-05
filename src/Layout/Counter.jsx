aef59cf22c19dae8bfe86850d4730a8e22328308
Elvis Makia <elvis_makia@yahoo.com>
2024-07-05 00:27:03 -0400

    added counter buttons  commit

--- src/Layout/Counter.jsx
+++ src/Layout/Counter.jsx
@@ -4,8 +4,10 @@ export default class Counter extends React.Component{
    render(){
     return(
 
-        <div>
-            Counter
+        <div className="row text-white">
+         <h1>Counter: </h1>   
+         <button style={{width:"200px"}}>+1</button>
+         <button style={{width: "200px"}}>-1</button>
         </div>
     );
    }
